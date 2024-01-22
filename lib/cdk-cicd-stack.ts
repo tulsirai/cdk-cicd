4dc5c1c63d6c03b306ad82740c5201c0068f5920
Tulsi Rai <tulsirai@gmail.com>
2024-01-22 08:21:16 -0800

    modified the configuration

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -14,7 +14,7 @@ export class CdkCicdStack extends cdk.Stack {
           'npm ci',
           'npx cdk synth'
         ],
-        primaryOutputDirectory: 'cdk.out'
+        // primaryOutputDirectory: 'cdk.out'
       })
     })
   }
