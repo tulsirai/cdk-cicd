ce570c9355430f47b3af32e7ca03a6d808b2752e
Tulsi Rai <tulsirai@gmail.com>
2024-01-21 22:56:21 -0800

    initial commit

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -5,12 +5,5 @@ import { Construct } from 'constructs';
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCicdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
--- test/cdk-cicd.test.ts
+++ test/cdk-cicd.test.ts
@@ -1,17 +1,3 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkCicd from '../lib/cdk-cicd-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-cicd-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkCicd.CdkCicdStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test('initial test', () => {
 });
